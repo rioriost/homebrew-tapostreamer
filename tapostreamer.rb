9f6581696b64d1ee90b1053cf96d26ee66daf262
Rio Fujita <rio_github@rio.st>
2025-02-05 22:48:59 +0900

    Update tapostreamer.rb

--- tapostreamer.rb
+++ tapostreamer.rb
@@ -2,38 +2,65 @@ class Tapostreamer < Formula
   include Language::Python::Virtualenv
 
   desc "TapoStreamer is an Python application that shows the video streaming from the cameras made by TP-Link."
-  homepage "https://github.com/rioriost/Tapo_Streamer/"
+  homepage "https://github.com/rioriost/tapostreamer/"
   url "https://files.pythonhosted.org/packages/44/0a/85ebc29fa76b458a967fc9f479e8a3669994df1b179ae689e74804a8bf9e/tapostreamer-0.1.0.tar.gz"
   sha256 "b49788b114c0ea0638ccc2ed0da89f85903c48b5ed0fba0b0edebfcbcd662914"
   license "MIT"
 
   depends_on "python@3.11"
 
-  resource "opencv-python" do
-    url "https://files.pythonhosted.org/packages/05/4d/53b30a2a3ac1f75f65a59eb29cf2ee7207ce64867db47036ad61743d5a23/opencv_python-4.11.0.86-cp37-abi3-macosx_13_0_arm64.whl"
-    sha256 "432f67c223f1dc2824f5e73cdfcd9db0efc8710647d4e813012195dc9122a52a"
+  resource "backports-tarfile" do
+    url "https://files.pythonhosted.org/packages/86/72/cd9b395f25e290e633655a100af28cb253e4393396264a98bd5f5951d50f/backports_tarfile-1.2.0.tar.gz"
+    sha256 "d75e02c268746e1b8144c278978b6e98e85de6ad16f8e4b0844a154557eca991"
   end
 
-  resource "numpy" do
-    url "https://files.pythonhosted.org/packages/9c/e6/efb8cd6122bf25e86e3dd89d9dbfec9e6861c50e8810eed77d4be59b51c6/numpy-2.2.2-cp311-cp311-macosx_14_0_arm64.whl"
-    sha256 "c7d1fd447e33ee20c1f33f2c8e6634211124a9aabde3c617687d8b739aa69eac"
+  resource "importlib-metadata" do
+    url "https://files.pythonhosted.org/packages/33/08/c1395a292bb23fd03bdf572a1357c5a733d3eecbab877641ceacab23db6e/importlib_metadata-8.6.1.tar.gz"
+    sha256 "310b41d755445d74569f993ccfc22838295d9fe005425094fad953d7f15c8580"
+  end
+
+  resource "jaraco-classes" do
+    url "https://files.pythonhosted.org/packages/06/c0/ed4a27bc5571b99e3cff68f8a9fa5b56ff7df1c2251cc715a652ddd26402/jaraco.classes-3.4.0.tar.gz"
+    sha256 "47a024b51d0239c0dd8c8540c6c7f484be3b8fcf0b2d85c13825780d3b3f3acd"
+  end
+
+  resource "jaraco-context" do
+    url "https://files.pythonhosted.org/packages/df/ad/f3777b81bf0b6e7bc7514a1656d3e637b2e8e15fab2ce3235730b3e7a4e6/jaraco_context-6.0.1.tar.gz"
+    sha256 "9bae4ea555cf0b14938dc0aee7c9f32ed303aa20a3b73e7dc80111628792d1b3"
+  end
+
+  resource "jaraco-functools" do
+    url "https://files.pythonhosted.org/packages/ab/23/9894b3df5d0a6eb44611c36aec777823fc2e07740dabbd0b810e19594013/jaraco_functools-4.1.0.tar.gz"
+    sha256 "70f7e0e2ae076498e212562325e805204fc092d7b4c17e0e86c959e249701a9d"
   end
 
   resource "keyring" do
-    url "https://files.pythonhosted.org/packages/d3/32/da7f44bcb1105d3e88a0b74ebdca50c59121d2ddf71c9e34ba47df7f3a56/keyring-25.6.0-py3-none-any.whl"
-    sha256 "552a3f7af126ece7ed5c89753650eec89c7eaae8617d0aa4d9ad2b75111266bd"
+    url "https://files.pythonhosted.org/packages/70/09/d904a6e96f76ff214be59e7aa6ef7190008f52a0ab6689760a98de0bf37d/keyring-25.6.0.tar.gz"
+    sha256 "0b39998aa941431eb3d9b0d4b2460bc773b9df6fed7621c2dfb291a7e0187a66"
+  end
+
+  resource "more-itertools" do
+    url "https://files.pythonhosted.org/packages/88/3b/7fa1fe835e2e93fd6d7b52b2f95ae810cf5ba133e1845f726f5a992d62c2/more-itertools-10.6.0.tar.gz"
+    sha256 "2cd7fad1009c31cc9fb6a035108509e6547547a7a738374f10bd49a09eb3ee3b"
+  end
+
+  resource "numpy" do
+    url "https://files.pythonhosted.org/packages/ec/d0/c12ddfd3a02274be06ffc71f3efc6d0e457b0409c4481596881e748cb264/numpy-2.2.2.tar.gz"
+    sha256 "ed6906f61834d687738d25988ae117683705636936cc605be0bb208b23df4d8f"
+  end
+
+  resource "opencv-python" do
+    url "https://files.pythonhosted.org/packages/17/06/68c27a523103dad5837dc5b87e71285280c4f098c60e4fe8a8db6486ab09/opencv-python-4.11.0.86.tar.gz"
+    sha256 "03d60ccae62304860d232272e4a4fda93c39d595780cb40b161b310244b736a4"
+  end
+
+  resource "zipp" do
+    url "https://files.pythonhosted.org/packages/3f/50/bad581df71744867e9468ebd0bcd6505de3b275e06f202c2cb016e3ff56f/zipp-3.21.0.tar.gz"
+    sha256 "2c9958f6430a2040341a52eb608ed6dd93ef4392e02ffe219417c1b28b5dd1f4"
   end
 
   def install
-    venv = virtualenv_create(libexec, "python3")
-
-    # Install each resource manually without --no-binary=:all:
-    resources.each do |r|
-      venv.pip_install r
-    end
-
-    # Install the main package
-    venv.pip_install_and_link buildpath
+    virtualenv_install_with_resources
   end
 
   test do
