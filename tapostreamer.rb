8f7f2e3a157591f967ebed30336667bda6511f19
Rio Fujita <rio_github@rio.st>
2025-02-06 10:53:19 +0900

    Update tapostreamer.rb

--- tapostreamer.rb
+++ tapostreamer.rb
@@ -3,8 +3,8 @@ class Tapostreamer < Formula
 
   desc "TapoStreamer is an Python application that shows the video streaming from the cameras made by TP-Link."
   homepage "https://github.com/rioriost/tapostreamer/"
-  url "https://files.pythonhosted.org/packages/44/0a/85ebc29fa76b458a967fc9f479e8a3669994df1b179ae689e74804a8bf9e/tapostreamer-0.1.0.tar.gz"
-  sha256 "b49788b114c0ea0638ccc2ed0da89f85903c48b5ed0fba0b0edebfcbcd662914"
+  url "https://files.pythonhosted.org/packages/f5/aa/6a774a1633efa0f1a842ec2fda641b1a572b37934f9a792d741cc95e944c/tapostreamer-0.1.1.tar.gz"
+  sha256 "15620e39029d685a6b088b18af30bc3a4b6a63bd52cc04082323dc16cf759e67"
   license "MIT"
 
   depends_on "python@3.11"
